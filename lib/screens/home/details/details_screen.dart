ed6fc598fce62851c0f417a10b44b868e09545d8
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-14 01:23:42 +0300

    final_for_now

--- lib/screens/home/details/details_screen.dart
+++ lib/screens/home/details/details_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:stylish/constants.dart';
 
 import '../../../models/Product.dart';
+import 'components/color_dot.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key, required this.product});
@@ -39,6 +40,9 @@ class DetailsScreen extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.4,
             fit: BoxFit.cover,
           ),
+          const SizedBox(
+            height: defaultPadding,
+          ),
           Expanded(
             child: Container(
               padding: const EdgeInsets.fromLTRB(
@@ -54,29 +58,84 @@ class DetailsScreen extends StatelessWidget {
                   topRight: Radius.circular(defaultBorderRadius * 3),
                 ),
               ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          product.title,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            product.title,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: defaultPadding,
+                        ),
+                        Text(
+                          '₺' + product.price.toString(),
                           style: Theme.of(context).textTheme.headlineSmall,
                         ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: defaultPadding,
                       ),
-                      const SizedBox(
-                        width: defaultPadding,
+                      child: Text(
+                        'A Henley shirt is a collarless pullover shirt, by a round neckline and a placket about 3 to 5 inches(8 to 13 cm) long and usually having 2-5 buttons.',
                       ),
-                      Text(
-                        '\₺' + product.price.toString(),
-                        style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const Text(
+                      'Colors',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
                       ),
-                    ],
-                  ),
-                  Text(
-                    'A Henley shirt is a collarless pullover shirt, by a round neckline and a placket about 3 to 5 inches(8 to 13 cm) long and usually having 2-5 buttons.',
-                  ),
-                ],
+                    ),
+                    const SizedBox(
+                      height: defaultPadding / 2,
+                    ),
+                    Row(
+                      children: [
+                        ColorDot(
+                          color: const Color(0xFFBEE8EA),
+                          isActive: false,
+                          press: () {},
+                        ),
+                        ColorDot(
+                          color: const Color(0xFF131B4A),
+                          isActive: true,
+                          press: () {},
+                        ),
+                        ColorDot(
+                          color: const Color(0xFFF4E5C3),
+                          isActive: false,
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: defaultPadding * 1.5,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: 200,
+                        height: 48,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 4,
+                            backgroundColor: primaryColor,
+                            shape: const StadiumBorder(),
+                          ),
+                          onPressed: () {},
+                          child: const Text('Add to Cart'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
